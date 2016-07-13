b5cb7ab2970d4187dd4a47eac1197a37354a2b58
Charlie Xu <xucharliey@gmail.com>
2016-07-13 19:58:40 -0400

    Added image preloading

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+$.fn.preload = function() {
+    this.each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+
+// Usage:
+
+$(['home-bg.png']).preload();
+
 $( document ).ready(function() {
 	$('#main-heading').imagesLoaded(function() {
 		$('#main-heading').fadeTo('slow',1);
-	});
-	
+	});	
 });
