3a008f75be8d69b26bd4afbc4254494577d7e492
Charlie Xu <xucharliey@gmail.com>
2016-07-14 23:57:18 -0400

    Fixed indentations

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-$.fn.preload = function() {
-    this.each(function(){
-        $('<img/>')[0].src = this;
-    });
-}
-
-// Usage:
-
-$(['home-bg.png']).preload();
-
 $( document ).ready(function() {
-	$('#main-heading').imagesLoaded(function() {
-		$('#main-heading').fadeTo('slow',1);
-	});	
+	$('#main-heading').fadeTo('slow',1);
 });
