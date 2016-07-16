10b94adebcae212449b1c5d55d32c866171c7565
Charlie Xu <xucharliey@gmail.com>
2016-07-15 22:50:55 -0400

    Added masonry for people

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 $( document ).ready(function() {
 	$('#main-heading').fadeTo('slow',1);
+	$('.grid').masonry({
+	  // options
+	  itemSelector: '.grid-item',
+	  columnWidth: 20,
+	  isFitWidth: true
+	});
 });
